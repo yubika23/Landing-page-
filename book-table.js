19fe91d5c97fb6ff1fe7763d518125efe264cde0
yubika23 <yubikachaudhary@gmail.com>
2025-06-07 11:24:22 +0530

    Add files via upload

---
+++ book-table.js
@@ -0,0 +1,10 @@
+<script>
+    // Get the close button element
+    const closeButton = document.getElementById('close-btn');
+
+    // Add an event listener to the close button
+    closeButton.addEventListener('click', function() {
+        // Redirect to another HTML page (for example, 'index.html')
+        window.location.href = 'file:///C:/Users/DELL/Desktop/Food/index.html'; // Replace with the actual URL or path of the HTML page you want to navigate to
+    });
+</script>
